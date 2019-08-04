b43e31276d29baf54601cca2ae648f19ee7a408d
Michael Gregoire <mdgregoire@gmail.com>
2019-08-04 13:46:44 -0500

    pc gets stuck when attempting to move through a wall

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -11,10 +11,15 @@ class GameMap:
         tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
 
         tiles[30][22].blocked = True
-        tiles[30][22].blocksight = True
+        tiles[30][22].block_sight = True
         tiles[31][22].blocked = True
         tiles[31][22].block_sight = True
         tiles[32][22].blocked = True
         tiles[32][22].block_sight = True
 
         return tiles
+
+    def is_blocked(self, x, y):
+        if self.tiles[x][y].blocked:
+            return True
+        return False
