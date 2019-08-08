68e5cebbc5da4a217388063e0a19ec37726763fc
Michael Gregoire <mdgregoire@gmail.com>
2019-08-08 11:47:56 -0500

    added ability to drop items from the inventory

--- input_handlers.py
+++ input_handlers.py
@@ -1,7 +1,36 @@
 import tcod as libtcod
 
+from game_states import GameStates
 
-def handle_keys(key):
+
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(key)
+
+    return {}
+
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt-Enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+
+    return {}
+
+
+def handle_player_turn_keys(key):
     key_char = chr(key.c)
     # Movement Keys
     if key.vk == libtcod.KEY_UP or key_char == 'k':
@@ -27,6 +56,9 @@ def handle_keys(key):
     if key_char == 'i':
         return {'show_inventory': True}
 
+    if key_char == 'd':
+        return {'drop_inventory': True}
+
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggles full screen
         return {'fullscreen': True}
@@ -37,3 +69,21 @@ def handle_keys(key):
     else:
         # No key presses
         return {}
+
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt-Enter: toggles full screen
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+
+    return {}
+
